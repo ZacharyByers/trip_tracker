2dad5ac239f55d633079e5fc422c1ea2404f222e
Zach Byers <byerszach@gmail.com>
2017-12-01 15:22:44 -0700

    add trip user relation

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,13 +1,49 @@
 class TripsController < ApplicationController
+  before_action :set_trip, only: [:show, :edit, :update, :destroy]
+
   def index
+    @trips = current_user.trips.all
   end
 
   def show
   end
 
   def new
+    @trip = current_user.trips.new
   end
 
   def edit
+  end
+
+  def create
+    @trip = current_user.trips.new(trip_params)
+    if @trip.save
+      redirect_to trips_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @trip.update(trip_params)
+      redirect_to trip_path(@trip)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @trip.destroy
+    redirect_to trips_path
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:name, :date)
+  end
+
+  def set_trip
+    @trip = current_user.trips.find(params[:id])
   end
 end
