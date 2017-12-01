dcdf68235fefd945a78376bae99c45bc5bc3e89f
isaac bustos <isaac.youthboard@futureindesign.org>
2017-12-01 16:56:30 -0700

    typo fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
   root 'trips#index'
   devise_for :users
-  resources :trips do
-    resources :locations, except: [:index]
-  end
-
-  scope 'locations/:location_id', as: 'location' do
-    resources :addresses
-  end
+  resources :trips
 end
