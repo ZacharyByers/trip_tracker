ad6da4aa2677283ba4709b5d1ef0a63318c74283
Uli12 <31193167+Uli12@users.noreply.github.com>
2017-12-04 09:08:50 -0700

    Add SCSS to trip and location show views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   root 'trips#index'
   devise_for :users
-  resources :trips
+  resources :trips do
+    resources :locations, except: [:index]
+  end
+
+  scope 'locations/:location_id', as: 'location' do
+    resources :addresses
+  end
 end
