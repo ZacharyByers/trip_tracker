eefada1f34968e0652831f557b7f35f696821216
Uli12 <31193167+Uli12@users.noreply.github.com>
2017-12-03 20:24:30 -0700

    Edit trip and location show view.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-2.times do
-  Trip.create(
-    name: Faker::StarTrek.location
-    date: Faker::Date.forward(150)
-     )
- end
-
-10.times do
-  Location.create(
-    name: Faker::Address.city,
-    trip_id: Faker::Number.between(0, 2),
-  )
- end
-
-10.times do
-  Address.create(
-    street_address: Faker::Address.street_address
-    zip: Faker::Address.zip_code
-    location_id: Faker::Number.between(1, 10)
-  )
- end
-  
